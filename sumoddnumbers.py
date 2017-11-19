b3614a7922f9a6382eea44702b2403ce4a247605
Kia Hamedi <kia.arta9793@gmail.com>
2017-11-19 21:25:19 +0330

    add script

---
+++ sumoddnumbers.py
@@ -0,0 +1,19 @@
+import random
+
+print("Enter Number of list:")
+nlist = int(input())
+numbers = []
+sum_odd = 0
+
+for num in range(nlist):
+    numbers.append(random.randint(0,nlist))
+
+print("This List:")
+print(numbers)
+
+print("Odd numbers in list:")
+for li in range(nlist):
+    if numbers[li]%2 != 0:
+        sum_odd += numbers[li]
+        print(numbers[li])
+print("Sum numbres of odd: "+ str(sum_odd))
